c78bae2214eb742abbd6b79690cf8ea07261ce61
Viru <v.kanjilal@ufl.edu>
2012-03-21 21:43:00 -0700

    Added graceful handling of ctrl+c

--- readTweets.py
+++ readTweets.py
@@ -6,25 +6,27 @@ import urllib2
 import time
 import sys
 import getopt
+import signal
 
 
-# Construct the procedure name, parameter list, and URL.
-
+# Construct the URLs and the constants. All time values are in secs
 url = 'http://localhost:8080/api/1.0/'
-
 STREAM_URL = "https://stream.twitter.com/1/statuses/sample.json"
-TIME_INTERVAL = 5
-NO_OF_WORKERS=10
-NO_OF_QUEUES = 5
+TIME_INTERVAL = 5     # time gap between the results    
+NO_OF_WORKERS = 10 
+q = [[],[],[],[],[]]       # internal data structre to buffer the tweets
+NO_OF_QUEUES = len(q)     
 SHOW_LEADERBOARD = False
 SHOW_TRENDINGTOPICS = False
-#q = Queue.Queue(0)
 count = 0
-q = [[],[],[],[],[]]
+threadlist = []  # list of threads in the program
 
+# some common words which has been avoided
 avoid = ['!', '?', 'que', 'on', 'of', 'in','I', 'q', 'and', 'in', 'for' , 'a', 'RT', ':', '...', 'my', 'me', '.', 'so', 'is','this', 'are', 'get', 'eu', 'just',',', 'de', 'i', 'that'] 
 totalTweets =  0
 
+
+# main calls to extract tweets from twitter
 class Client(threading.Thread):
 	def __init__(self, username, password, qu) :
 		self.friends = []
@@ -48,6 +50,7 @@ class Client(threading.Thread):
 		self.conn.perform()
 
 
+	# store the tweets in the buffer
 	def on_receive(self, data):
 		#global q
 		global totalTweets
@@ -61,27 +64,37 @@ class Client(threading.Thread):
 				self.q.append(u"{0[user][name]}: {0[text]}".format(content))
 
 	def run(self):
-		if self.authenticate(self.username, self.password):
-			self.connect()
+		try:
+			if self.authenticate(self.username, self.password):
+				self.connect()
+		except:
+			sys.exit(0)
 
 
-
+# worker thread to write to the database
 class Worker(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 		self.nextQIndex = 0
 
+	# extracts tweet from the buffer and parses it 
 	def run(self):
-		global q
-		while(True):
-			l = len(q[self.nextQIndex])
-			if(l == 0):
-				time.sleep(2)
-				continue 
-			tweet = q[self.nextQIndex].pop(0) #q.get(True)
-			self.parseAndAdd(tweet)
-			self.nextQIndex = (self.nextQIndex + 1) % NO_OF_QUEUES
+		try:
+			global q
+			while(True):
+				l = len(q[self.nextQIndex])
+				if(l == 0):
+					time.sleep(2)
+					continue 
+				tweet = q[self.nextQIndex].pop(0)
+				self.parseAndAdd(tweet)
+				self.nextQIndex = (self.nextQIndex + 1) % NO_OF_QUEUES
+		except:
+			sys.exit(0)
 
+	# Parses the word and invokes the procedure to write it to the database
+	# To make the process faster, instead of sending individual words, a list of word is sent 
+	# to the procedure which are individually written to the database 
 	def parseAndAdd(self, data):
 		global count
 		
@@ -100,11 +113,15 @@ class Worker(threading.Thread):
 			exit(1)
 		count+=length 
 
+
+# class to compute and display statistics and trending topics
 class statistics(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 		self.TotalCount = 0
 
+
+	# invokes the 'readstats' procedure to read the stats
 	def showLeaderBoard(self):
 		global count
 		global totalTweets
@@ -125,17 +142,16 @@ class statistics(threading.Thread):
 		print str(count/float(TIME_INTERVAL)) + " words per second"
 		#self.TotalCount = self.TotalCount + count 
 		count = 0
-		#print "Total words : " + str(self.TotalCount)
 		print "Total tweets : " + str(totalTweets)
 		print "Reads per sec :  " + str(reads/float(TIME_INTERVAL))
 		print "Writes per sec : " + str(writes/float(TIME_INTERVAL))
 		print "Updates per sec : " + str(updates/float(TIME_INTERVAL))
 
 
-
+	
+	# invokes the 'gettopics' procedure to get the trending topics
 	def showTrendingTopics(self):
 		httpparams = urllib.urlencode({'Procedure': 'gettopics'})
-		# Execute the request
 		data = []
 		# Execute the request
 		try:
@@ -152,19 +168,21 @@ class statistics(threading.Thread):
 		
 
 	def run(self):
-		global TIME_INTERVAL
-		global SHOW_LEADERBOARD
-		global SHOW_TRENDINGTOPICS
-		while(True):
-			time.sleep(TIME_INTERVAL)
-			print "Showing leaderboard"
-			if SHOW_LEADERBOARD == True:
-				self.showLeaderBoard()
-			print "Show trendingtopics"
-			if SHOW_TRENDINGTOPICS == True:
-				self.showTrendingTopics()
+		try:
+			global TIME_INTERVAL
+			global SHOW_LEADERBOARD
+			global SHOW_TRENDINGTOPICS
+			while(True):
+				time.sleep(TIME_INTERVAL)
+				if SHOW_LEADERBOARD == True:
+					self.showLeaderBoard()
+				if SHOW_TRENDINGTOPICS == True:
+					self.showTrendingTopics()
+		except:
+			sys.exit(0)
 
-
+# Class to purge the old words. Words older than 5 minutes are purged from the database
+# using the procedure 'purge'
 class purge(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
@@ -179,6 +197,9 @@ class purge(threading.Thread):
 			time.sleep(2*TIME_INTERVAL + 1)
 			self.purgefunction()
 
+
+# Function to parse the command line arguments and initializes 
+# the STATS table using the 'initialize' procedure
 def initialize():
 	global SHOW_LEADERBOARD
 	global SHOW_TRENDINGTOPICS
@@ -202,26 +223,52 @@ def initialize():
 			if item[1] == '':
 				continue
 			NO_OF_WORKERS = int(item[1])
-	print SHOW_TRENDINGTOPICS
 
-	
+
+# catch the CTRL+C signal and halt the program
+# very ugly way of terminating all the threads
+def signal_handler(signal, frame):
+	for thread in threadlist:
+	    if thread.isAlive():
+		try:
+		    thread._Thread__stop()
+		except:
+		    print str(thread.getName()) + ' could not be terminated'
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def main():
-	initialize()
-	statistics().start()
-	purge().start()
-	client1 = Client("__test_1", "kanjilal", q[0]).start()
-	client2 = Client("__test_2", "kanjilal", q[1]).start()
-	client3 = Client("__test_3", "kanjilal", q[2]).start()
-	client4 = Client("__test_4", "kanjilal", q[3]).start()
-	client5 = Client("__test_5", "kanjilal", q[4]).start()
+	global threadlist
 	
+	initialize()
+	# create the statistics thread and store it 
+	threadlist.append(statistics())
+
+	# create the purge thread and store it
+	threadlist.append(purge())
+
+	# Create 5 streams to extract tweets from Twitter
+	# 4 different accounts have been used and each of the streams have 
+	# been passed a buffer 
+	threadlist.append(Client("__test_1", "kanjilal", q[0]))
+	threadlist.append(Client("__test_2", "kanjilal", q[1]))
+	threadlist.append(Client("__test_3", "kanjilal", q[2]))
+	threadlist.append(Client("__test_4", "kanjilal", q[3]))
+	threadlist.append(Client("__test_5", "kanjilal", q[4]))
+	
+
+	# create the required number of worker threads
 	global NO_OF_WORKERS
 	print "Starting " + str(NO_OF_WORKERS) + " worker threads"
 	for x in xrange(NO_OF_WORKERS):
-		time.sleep(1)
-		Worker().start()
+		threadlist.append(Worker())
+		
+	# start all the threads
+	for thread in threadlist:
+		thread.start()
 
 
 if __name__ == '__main__':
 	main()
+	signal.pause()
